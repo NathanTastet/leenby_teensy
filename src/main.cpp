71593debf3baa2464024fcecf66e5bdc2eebcdb4
Nathan <nathan.tastet69@gmail.com>
2024-02-21 11:55:53 +0100

    mouvement

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 
 #define bras_gauche 0
 #define bras_droit 1
+#define TAILLE_MESSAGE_MAX 17
+#define VITESSE_MAX_DEG_S 360 // en réel on est à max 408 deg/s mais on met une marge de sécurité
 
 
 void individual_move(HerkulexServoBus bus,HerkulexServo servo, uint16_t pos, uint8_t playtime);
@@ -21,20 +23,18 @@ HerkulexServoBus herkulex_bus(Serial2);
 // id : 21 à 27, 31 à 27
 // création d'un tableau de servos
 
-/* HerkulexServo* tableauServo[2][7] = {{nullptr}};*/
+HerkulexServo* tableauServo[2][8] = {{nullptr}};
 
-HerkulexServo    servo_a(herkulex_bus, 36);
-HerkulexServo    servo_b(herkulex_bus, 37);
 HerkulexServo    servo_tout(herkulex_bus, SERVO_ID);
 
 void setup() {
 
   // initialiser les servos
-  /*for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 7; ++j) {
-            tableauServo[i][j] = new HerkulexServo(herkulex_bus,((i+2)*10+j)); // Crée un nouvel objet HerkulexServo avec le bon id
-        }
-    }*/
+  for (int i = 0; i < 2; ++i) {
+      for (int j = 0; j < 7; ++j) {
+          tableauServo[i][j] = new HerkulexServo(herkulex_bus,((i+2)*10+j)); // Crée un nouvel objet HerkulexServo avec le bon id
+      }
+  }
 
   // initialiser la liaison série
     Serial2.setTX(PIN_SW_TX);
@@ -42,11 +42,6 @@ void setup() {
     Serial.begin(9600);
     Serial2.begin(666666);
     delay(500);
-
-  // créer un écouteur d'évenements sur serial, pour lire si des infos servos sont recues
-  //Serial2.onReceive(gestion_interruption);
-  
-  // il faut faire attention dès qu'on recoit un octet qui correspond à --> "D" en ascii (0x44) soit 68 en décimal
   
   // turn power on
   servo_tout.setTorqueOn();
@@ -67,29 +62,65 @@ void loop() {
   herkulex_bus.executeMove();
   herkulex_bus.update();
   delay(100 * 11.2f);*/
-  
-  servo_a.setLedColor(HerkulexLed::Cyan);
-  servo_b.individual_move(256,100, HerkulexLed::Yellow);
 
-  servo_a.setLedColor(HerkulexLed::Purple);
-  servo_b.individual_move(1024,100, HerkulexLed::Yellow);
+  // il faut faire attention dès qu'on recoit un octet qui correspond à --> "D" en ascii (0x44) soit 68 en décimal
+  // sur serial
+  if(Serial.available()){
+    // on lit l'octet
+    char octetlu = Serial.read();
+    // on traite l'octet
+    if(octetlu ==  68){
+      // on appelle la fonction de gestion d'interruption
+      interruption();
+    }
+  }
 
-  servo_b.setLedColor(HerkulexLed::White);
-  servo_a.individual_move(256,100, HerkulexLed::Yellow);
-
-  servo_b.setLedColor(HerkulexLed::Green);
-  servo_a.individual_move(1024,100, HerkulexLed::Yellow);
   
 }
 
 // gestion d'interruptions
-void gestion_interruption(){
-  // création d'un tableau pour stocker les valeurs
-
+void interruption(){
+  while(Serial.available()< TAILLE_MESSAGE_MAX){
+    // on attend de recevoir les 16 octets du message complet
+  }
+  char octets_recus[TAILLE_MESSAGE_MAX];
+  // on lit les 16 octets et on les stocke dans un tableau
+  for(int i = 0; i <TAILLE_MESSAGE_MAX; i++){
+    octets_recus[i] = Serial.read();
+    // si on recoit un octet qui correspond à "F" en ascii (0x46) soit 70 en décimal
+    // on sort de la boucle (normalement c'est le dernier octet du message reçu)
+    if(octets_recus[i] == 70){
+      break;
+    } 
+  }
+  
   // lecture des octets un par un jusqu'à l'octet de stop "F" soit 0x46 en ascii 70 en décimal
-
   // traitement des octets reçus 
-  // commandes à executer en fonction des valeurs recues
+
+  // 17 octets :
+  // 1 octet pour le choix bras en short [0]
+  // 14 octets pour les 7 angles en long de [1] à [14]
+  // 1 octet pour la vitesse en short [15]
+  // 1 octet de stop [16]
+
+  char bras = octets_recus[0];
+  char vitessept = octets_recus[15];
+  // on a une vitesse en % de la vitesse max
+  // on a une vitesse max de 360 deg/s
+  char vitesse = (vitessept/100)*VITESSE_MAX_DEG_S;
+
+  // les angles sont sur 2 octets uint_16
+
+
+  for(int i = 1; i < 7; i+=2){
+    tableauServo[bras][i]->setLedColor(HerkulexLed::Green);
+    uint16_t angle_degre = (octets_recus[i] << 8) | octets_recus[i+1];
+    uint16_t tps_ms = (angle_degre/vitesse)*1000; // calcul du temps en s puis x1000 pour avoir le temps en ms
+    tableauServo[bras][i]->individual_move(angle_degre,tps_ms, HerkulexLed::Yellow);
+  }
+
+
+  
 }
 
  
