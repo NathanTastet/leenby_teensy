eebc7fbe92aa5dd3533a2e98864d79028af0311d
Nathan <nathan.tastet69@gmail.com>
2024-02-21 08:23:18 +0100

    bonjour

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@
 #define PIN_SW_RX  9
 #define PIN_SW_TX  10
 #define SERVO_ID_A 36
-
-
+#define SERVO_ID_B 37
+#define SERVO_ID 254
+void individual_move(HerkulexServoBus bus,HerkulexServo servo, uint16_t pos, uint8_t playtime);
 
 HerkulexServoBus herkulex_bus(Serial2);
 HerkulexServo    servo_a(herkulex_bus, SERVO_ID_A);
-
-
+HerkulexServo    servo_b(herkulex_bus, SERVO_ID_B);
+HerkulexServo    servo_tout(herkulex_bus, SERVO_ID);
 
 void setup() {
     Serial2.setTX(PIN_SW_TX);
@@ -22,18 +23,37 @@ void setup() {
 
   // turn power on
   servo_a.setTorqueOn();
-  
 
-  herkulex_bus.prepareIndividualMove();
-  servo_a.setPosition(256, 50,HerkulexLed::Purple);
-  herkulex_bus.executeMove();
-
+ // individual_move(herkulex_bus,servo_a, 256, 50);
   delay(100 * 11.2f);
-
-  // turn power off
-  servo_a.setTorqueOff();
 }
 
 void loop() {
+  
+  /*herkulex_bus.prepareIndividualMove();
+  servo_a.setPosition(256, 50, HerkulexLed::Cyan);
+  herkulex_bus.executeMove();
   herkulex_bus.update();
+  delay(100 * 11.2f);
+  herkulex_bus.prepareIndividualMove();
+  servo_a.setPosition(1024, 50, HerkulexLed::Purple);
+  herkulex_bus.executeMove();
+  herkulex_bus.update();
+  delay(100 * 11.2f);*/
+
+  servo_tout.setLedColor(HerkulexLed::Cyan);
+  delay(100);
+  servo_tout.setLedColor(HerkulexLed::Purple);
+  delay(100);
 }
+
+ void individual_move(HerkulexServoBus bus,HerkulexServo servo, uint16_t pos, uint8_t playtime) {
+  
+  bus.prepareIndividualMove();
+  servo.setPosition(pos,playtime, HerkulexLed::Purple);
+  bus.executeMove();
+
+ }
+
+
+ 
